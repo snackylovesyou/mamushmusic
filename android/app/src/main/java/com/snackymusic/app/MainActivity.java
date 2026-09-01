bbb8f60a792df21c3229ed4ebc6c5f52b01ba76d
snackylovesyou <beltranpvel@gmail.com>
2026-08-31 20:46:51 -0600

    Hack en Java para forzar música en segundo plano

--- android/app/src/main/java/com/snackymusic/app/MainActivity.java
+++ android/app/src/main/java/com/snackymusic/app/MainActivity.java
@@ -1,5 +1,18 @@
 package com.snackymusic.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onPause() {
+        super.onPause();
+        
+        // ¡EL HACK MAESTRO!: Cuando Android intenta pausar la app, 
+        // forzamos al WebView (el reproductor) a mantenerse despierto.
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().onResume();
+            this.bridge.getWebView().resumeTimers();
+        }
+    }
+}
